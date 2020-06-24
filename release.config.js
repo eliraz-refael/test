393cb39311e75d0373c229d25b60c8fd345f511f
Eliraz Kedmi <eliraz.kedmi@whitesourcesoftware.com>
2020-06-24 10:19:00 +0300

    feat(build): fixed semantic release

--- release.config.js
+++ release.config.js
@@ -1,4 +1,10 @@
 module.exports =  {
     repositoryUrl: 'https://www.github.com/eliraz-refael/test',
-    branches: ['master', 'next']
+    branches: ['master', 'next'],
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        // "@semantic-release/npm",
+        "@semantic-release/git"
+  ]
 }
