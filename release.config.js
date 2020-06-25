2d2a7cb685ba47add50a6471951e990fc8b60b79
Eliraz Kedmi <eliraz.kedmi@whitesourcesoftware.com>
2020-06-25 11:09:43 +0300

    feat(build): adding integration branch

--- release.config.js
+++ release.config.js
@@ -1,6 +1,9 @@
 module.exports =  {
     repositoryUrl: 'https://www.github.com/eliraz-refael/test',
-    branches: ['master', 'next'],
+    branches: [
+        {name: 'master'},
+        {name: 'integration', channel: 'next', prerelease: 'rc'}, // `prerelease` is set to `beta` as it is the value of `name`
+    ],
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
